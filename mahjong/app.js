c5f8042cdf3ee7f06a143f11f8a7abcff89a138c
SK <59910993+TikuisChan@users.noreply.github.com>
2020-08-09 23:46:16 +0800

    bug fix, local memory storage
    fix bug of not able to change player names
    store records into local memory, records won't lose unless clear all button was pressed

--- mahjong/app.js
+++ mahjong/app.js
@@ -8,34 +8,34 @@ const money = document.querySelector('.moneyInput');
 const submit = document.querySelector('.submit-button');
 const allRecord = document.querySelector('.record tbody');
 const summary = document.querySelector('.total-points').getElementsByTagName("td");
+const clearBtn = document.querySelector('.clear-button');
 
 // points record
-summaryPt = [0, 0, 0, 0];
+var summaryPt = [0, 0, 0, 0];
 
 // event listeners
+document.addEventListener('DOMContentLoaded', getRecords);
 playerNameButton.addEventListener('click', namePlayers);
 submit.addEventListener('click', addRecord);
-
 var playerwin
 for (playerwin of playersWin) {
     playerwin.addEventListener('click', win);
 }
-
 var playerlose
 for (playerlose of playersLose) {
     playerlose.addEventListener('click', lose);
 }
-
 allRecord.addEventListener('click', deleteClick);
+clearBtn.addEventListener('click', clearAll);
 
 // functions
 function namePlayers (event){
     event.preventDefault();
-    var players = playerNames.childNodes;
-    players[1].innerHTML = player1Name.value;
-    players[3].innerHTML = player2Name.value;
-    players[5].innerHTML = player3Name.value;
-    players[7].innerHTML = player4Name.value;
+    const players = playerNames.getElementsByTagName("th");
+    players[0].innerHTML = playersName[0].value;
+    players[1].innerHTML = playersName[1].value;
+    players[2].innerHTML = playersName[2].value;
+    players[3].innerHTML = playersName[3].value;
 }
 
 function clearWin () {
@@ -75,29 +75,33 @@ function addRecord (event) {
 
 function writeRec (win, lose, m) {
     var newPlayersRecords = newRecordsfunc();
-
+    var record = [0, 0, 0, 0];
     newPlayersRecords[win].innerHTML = m;
     summaryPt[win] += m;
-
+    record[win] = m;
     switch (lose.length) {
         case 1:
             newPlayersRecords[lose[0]].innerHTML = -m;
             summaryPt[lose[0]] += -m;
+            record[lose[0]] += -m;
+            // newRecord[lose[0]] += -m;
             break;
         case 3:
             for (i of lose) {
                 newPlayersRecords[i].innerHTML = -m / 3;
                 summaryPt[i] += -m / 3;
+                record[i] += -m / 3;
             }
             break;
     }
-
     var newRecord = document.createElement('tr');
+    newRecord.classList.add('single-record');
     for (i of newPlayersRecords) {
         newRecord.appendChild(i);
     }
     allRecord.appendChild(newRecord);
-    updateTotalPoints()
+    saveToLocal(record);
+    updateTotalPoints();
 }
 
 function searchWinner() {
@@ -146,9 +150,35 @@ function deleteClick (e) {
     const item = e.target;
     if (item.classList.contains('trash-btn')) {
         removeItem = item.closest("tr").getElementsByTagName("td");
+        var delRow = [0, 0, 0, 0];
         for (i = 0; i < 4; i++) {
             summaryPt[i] -= parseInt(removeItem[i].innerText);
+            delRow[i] = parseInt(removeItem[i].innerText);
         }
+        let MJRecords;
+        // check local storage for MJRecords
+        if (localStorage.getItem("MJRecords") === null) {
+            MJRecords = [];
+        } else {
+            MJRecords = JSON.parse(localStorage.getItem("MJRecords"));
+        }
+        var i = 0;
+        while (i < MJRecords.length) {
+            var equal = 0;
+            var j = 0;
+            while (j < MJRecords[i].length) {
+                if (MJRecords[i][j] === delRow[j]) {
+                    equal += 1;
+                }
+                ++j;
+            }
+            if (equal === 4) {
+                MJRecords.splice(i, 1);
+            } else {
+                ++i;
+            }
+        }
+        localStorage.setItem("MJRecords", JSON.stringify(MJRecords));
         item.closest("tr").remove();
         updateTotalPoints();
     }
@@ -159,4 +189,59 @@ function updateTotalPoints () {
     for (i = 0; i < 4; i++) {
         summary[i].innerText = summaryPt[i];
     }
+}
+
+function saveToLocal (record) {
+    let MJRecords;
+    // check local storage for MJRecords
+    if (localStorage.getItem("MJRecords") === null) {
+        MJRecords = [];
+    } else {
+        MJRecords = JSON.parse(localStorage.getItem("MJRecords"));
+    }
+    // push record into MJRecords
+    MJRecords.push(record);
+    localStorage.setItem("MJRecords", JSON.stringify(MJRecords));
+}
+
+function clearAll (event) {
+    event.preventDefault();
+    const players = playerNames.getElementsByTagName("th");
+    var i;
+    for (i = 0; i < 4; i++) {
+        players[i].innerHTML = 'Player' + (i + 1).toString();
+        summary[i].innerText = 0;
+    }
+    const records = document.querySelectorAll('.single-record');
+    records.forEach((element) => {
+            element.remove();
+        })
+    clearWin();
+    clearLose();
+    localStorage.clear();
+}
+
+function getRecords () {
+    let MJRecords;
+    // check local storage for MJRecords
+    if (localStorage.getItem("MJRecords") === null) {
+        MJRecords = [];
+    } else {
+        MJRecords = JSON.parse(localStorage.getItem("MJRecords"));
+    }
+    MJRecords.forEach(function(element) {
+        var newPlayersRecords = newRecordsfunc();
+        var i;
+        for (i = 0; i < 4; i++) {
+            newPlayersRecords[i].innerHTML = element[i];
+            summaryPt[i] += element[i];
+        }
+        var newRecord = document.createElement('tr');
+        newRecord.classList.add('single-record');
+        for (i of newPlayersRecords) {
+            newRecord.appendChild(i);
+        }
+        allRecord.appendChild(newRecord);
+        updateTotalPoints();
+    })
 }
